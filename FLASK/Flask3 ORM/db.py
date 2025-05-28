2c46fb1e5b8b0be80964ab2139a69d2bff7caa3d
rmg <rmgajdosik@gmail.com>
2025-05-28 22:46:52 +0200

    Revert "Moved to separate adressary"
    This reverts commit 7cc614785ae377828d94b0963277900870b14de9.

--- FLASK/Flask3 ORM/db.py
+++
@@ -1,6 +0,0 @@
-from flask_alchemy import SQLAlchemy
-from flask import Flask
-
-app = Flask(__name__)
-
-db = SQLAlchemy(app)
--- FLASK/Flask3 ORM/members.py
+++
@@ -1,9 +0,0 @@
-from  app import db
-
-class Member(db.Model):
-    __tablename__ = 'members'
-    member_id = db.Column(db.Integer, primary_key = True)
-    first_name = db.Column(db.String(255))
-    last_name = db.Column(db.String(255))
-    email = db.Column(db.String(255), unique=True)
-    registration_date = db.Column(db.DateTime)
